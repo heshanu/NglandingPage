3aec5c2159a9e2acd11e8636ee64e859658b93b0
heshan umayanga <heshanu97@gmail.com>
2022-03-19 12:33:10 +0530

    Added compoents forecast

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ForecastComponent } from './forecast/forecast.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ForecastComponent
+  ],
   imports: [
     CommonModule
   ]
