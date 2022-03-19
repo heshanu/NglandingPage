5eb841fff8d6c88c4be3852628251ceaae688ce2
heshan umayanga <heshanu97@gmail.com>
2022-03-19 14:44:13 +0530

    3/19-2.45

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import {HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
-  constructor() { }
+  constructor() { 
+    this.getCurrentLocation();
+  }
+
+  
+  getForecast(){
+    return this.getCurrentLocation()
+      .pipe(
+        map(coords=>{
+          return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'metric')
+          .set('appid', '9581ec5ca8cf49c7abdde2360a727383')
+        })
+    )
+  }
   getCurrentLocation(){
     
     return new Observable<GeolocationCoordinates>((observer)=>{
